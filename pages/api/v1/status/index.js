09f1360cbaacfac28d83dae749c4895bf2180453
Robert Carvalho <robert.carvalho@educare.digital>
2025-04-30 12:07:26 -0300

    feat(index): returns database status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,32 @@
-import database from 'infra/database.js';
+import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query('SELECT 1+1 as sum;');
-  console.log(result.rows);
-  response.status(200).json({mensagem: "Salve meus nobres, cãos"})
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+
+  const pgVersionQueryResponse = await database.query("SHOW server_version;");
+  const pgVersion = pgVersionQueryResponse.rows[0].server_version;
+
+  const maxConnectionsQueryResponse = await database.query(
+    "SHOW max_connections;",
+  );
+  const maxConnections = maxConnectionsQueryResponse.rows[0].max_connections;
+
+  const activeConnectionsQueryResponse = await database.query({
+    text: "SELECT count(*)::int as active_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const activeConnections =
+    activeConnectionsQueryResponse.rows[0].active_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: pgVersion,
+        max_connections: parseInt(maxConnections),
+        active_connections: activeConnections,
+      },
+    },
+  });
 }
