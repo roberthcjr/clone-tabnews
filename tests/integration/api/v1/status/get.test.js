c8b210682cd927aa9058605e953a4ea1a68a38c5
Robert Carvalho <robert.carvalho@educare.digital>
2025-04-30 12:07:44 -0300

    test(get): adds tests to database status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,12 +1,52 @@
 test("getting from /api/v1/status, should return 200", async () => {
   const response = await fetch('http://localhost:3000/api/v1/status');
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 });
-test("getting from /api/v1/status, should return expected message", async () => {
-  const expectedMessage = "Salve meus nobres, cãos";
+
+test("getting from /api/v1/status, should return valid date in updated_at", async () => {
+  const response = await fetch('http://localhost:3000/api/v1/status');
+  const responseBody = await response.json();
+  
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
+
+test("getting from /api/v1/status, should return 16 in version", async () => {
+  const expectedVersion = "16.0";
 
   const response = await fetch('http://localhost:3000/api/v1/status');
-  const body = await response.json();
-  
-  expect(body.mensagem).toBe(expectedMessage);
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.version).toBe(expectedVersion);
+});
+
+test("getting from /api/v1/status, should return 100 in max_connections", async () => {
+  const expectedVersion = 100;
+
+  const response = await fetch('http://localhost:3000/api/v1/status');
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.max_connections).toBe(expectedVersion);
+});
+
+test("getting from /api/v1/status, should return number in active_connections", async () => {
+  const response = await fetch('http://localhost:3000/api/v1/status');
+  const responseBody = await response.json();
+
+  const parsedActiveConnections = Number(responseBody.dependencies.database.active_connections)
+
+  expect(responseBody.dependencies.database.active_connections).toEqual(parsedActiveConnections);
+});
+
+test("getting from /api/v1/status, should return 1 in active_connections", async () => {
+  const response = await fetch('http://localhost:3000/api/v1/status');
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.active_connections).toBe(1);
 });
