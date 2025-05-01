7e6b8c858bbaf8c236be7520ec22fe223a6bbbba
Robert Carvalho <robert.carvalho@educare.digital>
2025-05-01 11:14:12 -0300

    revert

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,45 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function MyApp() {
+  const [topPosition, setTopPosition] = useState(0);
+  const [leftPosition, setLeftPosition] = useState(0);
+
+  const changeNoPosition = () => {
+    const maxWidth = window.innerWidth - 100;
+    const maxHeight = window.innerHeight - 50;
+
+    const newTop = Math.floor(Math.random() * maxHeight);
+    const newLeft = Math.floor(Math.random() * maxWidth);
+
+    setTopPosition(newTop);
+    setLeftPosition(newLeft);
+  };
+
+  const result = () => {
+    alert("Eu também te amo!");
+  };
 
   return (
-      <h1>Oi, amor. Se você me ama, dá uma risadinha</h1>
+    <div className="main_frame">
+      <div className="content">
+        <div className="header">
+          <h1>Oi, amor. Você me ama?</h1>
+        </div>
+        <div className="body">
+          <button onClick={result}>Sim</button>
+          <button
+            onMouseOver={changeNoPosition}
+            style={
+              topPosition && leftPosition
+                ? { top: topPosition + "px", left: leftPosition + "px" }
+                : { "margin-right": "-200px" }
+            }
+          >
+            Não
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
