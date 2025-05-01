9abf87b9a12326cbd6305b0d56060f28da7f52ef
Robert Carvalho <robert.carvalho@educare.digital>
2025-05-01 11:46:43 -0300

    fix(database): makes errors to throw

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,17 @@
 import { Client } from "pg";
 
+const PG_CREDENTIALS = {
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+}
+
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  });
+  const client = new Client(PG_CREDENTIALS);
+
+  console.log(PG_CREDENTIALS);
   try {
     await client.connect();
 
@@ -16,6 +20,7 @@ async function query(queryObject) {
     return response;
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
