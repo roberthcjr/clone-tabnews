d57ddbbd4239421b983b854c8b550966ec5ac1d4
Robert Carvalho <roberthcjr1999@gmail.com>
2025-05-06 11:49:23 -0300

    fix: adds treatment to not accepted methods

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,35 +2,47 @@ import database from "infra/database.js";
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
+const ACCEPTED_METHODS = ["GET", "POST"];
+
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationsRunnerOptions = {
-    databaseUrl: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-  if (request.method === "GET") {
-    console.log("Entrou no GET");
-    const migrations = await migrationRunner(defaultMigrationsRunnerOptions);
-    await dbClient.end();
-    return response.status(200).json(migrations);
-  }
-  if (request.method === "POST") {
-    console.log("Entrou no POST");
-    const migrationsMigrated = await migrationRunner({
-      ...defaultMigrationsRunnerOptions,
-      dryRun: false,
+  if (!ACCEPTED_METHODS.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
     });
-
-    await dbClient.end();
-
-    if(migrationsMigrated.length > 0) return response.status(201).json(migrationsMigrated)
-    
-    return response.status(200).json(migrationsMigrated);
   }
 
-  return response.status(405).end();
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationsRunnerOptions = {
+      databaseUrl: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+    if (request.method === "GET") {
+      console.log("Entrou no GET");
+      const migrations = await migrationRunner(defaultMigrationsRunnerOptions);
+      return response.status(200).json(migrations);
+    }
+    if (request.method === "POST") {
+      console.log("Entrou no POST");
+      const migrationsMigrated = await migrationRunner({
+        ...defaultMigrationsRunnerOptions,
+        dryRun: false,
+      });
+
+      if (migrationsMigrated.length > 0)
+        return response.status(201).json(migrationsMigrated);
+
+      return response.status(200).json(migrationsMigrated);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
---
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -0,0 +1,5 @@
+test("DELETE to /api/v1/migrations, should return status 405", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {method: "DELETE"});
+
+  expect(response.status).toBe(405);
+});
