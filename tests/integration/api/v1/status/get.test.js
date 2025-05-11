5885f9ce0e287b24282995bc1a68aca85a2a4e1c
Robert Carvalho <roberthcjr1999@gmail.com>
2025-05-11 19:59:17 -0300

    test action

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -16,6 +16,8 @@ test("GET to /api/v1/status, should return 200", async () => {
 });
 
 test("GET to /api/v1/status, should return valid date in updated_at", async () => {
+
+  
   const response = await fetch("http://localhost:3000/api/v1/status");
   const responseBody = await response.json();
 
