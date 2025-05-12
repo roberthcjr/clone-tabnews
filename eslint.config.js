7e7432a030c2993e64f741482aaedee7fde507f0
Robert Carvalho <roberthcjr1999@gmail.com>
2025-05-11 21:00:20 -0300

    build: adds eslint to actions

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+
+export default defineConfig([
+  {
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    files: ["**/*.js"],
+    plugins: {
+      js,
+      react,
+    },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+    },
+  },
+  globalIgnores(["**/.next/"]),
+]);
