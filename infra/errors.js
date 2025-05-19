d2704b779c4f8ca136e6d1769c17ea9a0201dd6d
Robert Carvalho <roberthcjr1999@gmail.com>
2025-05-19 10:40:53 -0300

    feat: create `InternalServerError`

---
+++ infra/errors.js
@@ -0,0 +1,17 @@
+export class InternalServerError extends Error {
+  constructor({ cause }, statusCode) {
+    super("Um erro interno inesperado aconteceu", { cause });
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte";
+    this.statusCode = statusCode ?? 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
