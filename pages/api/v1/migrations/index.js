6ddd971ff7e062b75702027315e465bb16d9e6da
Robert Carvalho <roberthcjr1999@gmail.com>
2025-05-19 12:22:01 -0300

    feat: implement `next-connect` to migrations endpoints

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,48 +1,68 @@
-import database from "infra/database.js";
-import migrationRunner from "node-pg-migrate";
+import { createRouter } from "next-connect";
 import { resolve } from "node:path";
+import migrationRunner from "node-pg-migrate";
 
-const ACCEPTED_METHODS = ["GET", "POST"];
+import database from "infra/database.js";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors.js";
 
-export default async function migrations(request, response) {
-  if (!ACCEPTED_METHODS.includes(request.method)) {
-    return response.status(405).json({
-      error: `Method "${request.method}" not allowed`,
-    });
-  }
+const router = createRouter();
 
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const defaultMigrationsRunnerOptions = {
-      databaseUrl: dbClient,
+router.get(getMigrations).post(runMigrations);
+export default router.handler({
+  onError: onErrorHandler,
+  onNoMatch: onNoMatchHandler,
+});
+
+async function getMigrations(request, response) {
+  const migrations = await migrationRunner(
+    await getDbConfiguration({ isDryRun: true }),
+  );
+  return response.status(200).json(migrations);
+}
+
+async function runMigrations(request, response) {
+  const migrationsMigrated = await migrationRunner(
+    await getDbConfiguration({}),
+  );
+
+  if (migrationsMigrated.length > 0)
+    return response.status(201).json(migrationsMigrated);
+
+  return response.status(200).json(migrationsMigrated);
+}
+
+function onNoMatchHandler(request, response) {
+  const publicError = new MethodNotAllowedError();
+  return response.status(publicError.statusCode).json(publicError);
+}
+
+function onErrorHandler(error, request, response) {
+  console.log("\n Erro dentro do catch do next-connect");
+  console.error(error);
+
+  let publicError = error;
+
+  if (!(publicError instanceof InternalServerError))
+    publicError = new InternalServerError({ error });
+
+  return response.status(publicError.statusCode).json(publicError);
+}
+
+async function getDbConfiguration({ isDryRun = false }) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationsRunnerOptions = {
+    databaseUrl: dbClient,
+    dryRun: false,
+    dir: resolve("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+  if (isDryRun)
+    return {
+      ...defaultMigrationsRunnerOptions,
       dryRun: true,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     };
-    if (request.method === "GET") {
-      console.log("Entrou no GET");
-      const migrations = await migrationRunner(defaultMigrationsRunnerOptions);
-      return response.status(200).json(migrations);
-    }
-    if (request.method === "POST") {
-      console.log("Entrou no POST");
-      const migrationsMigrated = await migrationRunner({
-        ...defaultMigrationsRunnerOptions,
-        dryRun: false,
-      });
 
-      if (migrationsMigrated.length > 0)
-        return response.status(201).json(migrationsMigrated);
-
-      return response.status(200).json(migrationsMigrated);
-    }
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    await dbClient.end();
-  }
+  return defaultMigrationsRunnerOptions;
 }
