9d5a9e339267f146f12e2a54b92fbaf61ec48fd7
Robert Carvalho <roberthcjr1999@gmail.com>
2025-05-21 13:06:51 -0300

    feat: validate unique `username` and `email`

--- models/user.js
+++ models/user.js
@@ -1,18 +1,73 @@
 import database from "infra/database.js";
+import { ValidationError } from "infra/errors.js";
 
 async function create({ username, email, password }) {
-  const result = await database.query({
-    text: `INSERT INTO 
-            users (username, email, password)
-          VALUES
-            ($1, $2, $3)
-          RETURNING
-            *
-          ;`,
-    values: [username, email, password],
-  });
+  await validateUniqueEmail(email);
+  await validateUniqueUsername(username);
 
-  return result.rows[0];
+  const newUser = await runInsertQuery({ username, email, password });
+  return newUser;
+
+  async function validateUniqueUsername(username) {
+    const result = await database.query({
+      text: `
+      SELECT
+        username
+      FROM
+        users
+      WHERE
+        LOWER(username) = LOWER($1)
+      ;`,
+      values: [username],
+    });
+
+    if (result.rowCount > 0) {
+      throw new ValidationError(
+        "O username informado já está sendo utilizado",
+        {
+          action: "Informe algum outro username",
+          statusCode: 409,
+        },
+      );
+    }
+  }
+
+  async function validateUniqueEmail(email) {
+    const result = await database.query({
+      text: `
+      SELECT
+        email, username
+      FROM
+        users
+      WHERE
+        LOWER(email) = LOWER($1)
+      ;`,
+      values: [email],
+    });
+
+    if (result.rowCount > 0) {
+      throw new ValidationError("O email informado já está sendo utilizado", {
+        action: "Informe algum outro email",
+        statusCode: 409,
+      });
+    }
+  }
+
+  async function runInsertQuery({ username, email, password }) {
+    const result = await database.query({
+      text: `
+      INSERT INTO 
+        users (username, email, password)
+      VALUES
+        ($1, $2, $3)
+      RETURNING
+        *
+      ;`,
+      values: [username, email, password],
+    });
+
+    return result.rows[0];
+  }
 }
 
 const user = {
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -14,7 +14,7 @@ beforeAll(async () => {
 
 describe("POST to api/v1/users", () => {
   describe("Anonymous user", () => {
-    test("With unique and valid data", async () => {
+    test("With unique and valid data, should return success", async () => {
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
         headers: {
@@ -42,6 +42,84 @@ describe("POST to api/v1/users", () => {
 
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+
+    test("With duplicated email, should return duplicated error", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "emailDuplicado1",
+          email: "emailDuplicado@email.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "emailDuplicado2",
+          email: "EmailDuplicado@email.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(response2.status).toBe(409);
+
+      const responseBody2 = await response2.json();
+
+      expect(responseBody2).toEqual({
+        name: "ValidationError",
+        message: "O email informado já está sendo utilizado",
+        action: "Informe algum outro email",
+        status_code: 409,
+      });
+    });
+
+    test("With duplicated username, should return duplicated error", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "usernameDuplicado",
+          email: "usernameDuplicado1@email.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "UsernameDuplicado",
+          email: "usernameDuplicado2@email.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(response2.status).toBe(409);
+
+      const responseBody2 = await response2.json();
+
+      expect(responseBody2).toEqual({
+        name: "ValidationError",
+        message: "O username informado já está sendo utilizado",
+        action: "Informe algum outro username",
+        status_code: 409,
+      });
     });
   });
 });
