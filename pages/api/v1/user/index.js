9c5b45979920b041642a99b53276b5630687488a
Robert Carvalho <roberthcjr1999@gmail.com>
2025-08-10 20:32:39 -0300

    feat: create `user` get route

---
+++ pages/api/v1/user/index.js
@@ -0,0 +1,20 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller";
+import session from "models/session";
+import user from "models/user.js";
+
+const router = createRouter();
+
+router.get(getHandler);
+export default router.handler(controller);
+
+async function getHandler(request, response) {
+  const sessionToken = request.cookies.session_id;
+
+  const sessionObject = await session.findOneValidByToken(sessionToken);
+  const renewedSessionObject = await session.renew(sessionObject.id);
+  await controller.setSessionCookie(renewedSessionObject.token, response);
+
+  const userFound = await user.findOneById(sessionObject.user_id);
+  return response.status(200).json(userFound);
+}
